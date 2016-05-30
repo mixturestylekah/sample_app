0c73bfe5bc4f5f1d666a987745cea7621b43ba69
kentaro <mixture.style.k.a.h@gmail.com>
2016-05-30 11:56:26 +0900

    add users controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
@@ -8,6 +9,16 @@ class StaticPagesControllerTest < ActionController::TestCase
 
   test "should get help" do
     get :help
+    assert_response :success
+  end
+
+  test "should get help" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
     assert_response :success
   end
 
